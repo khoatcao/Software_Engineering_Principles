2fa9769ae53a579e9ca571cb86bcae237c961d6b
khoa <caotoankhoa5@gmail.com>
2021-09-01 16:15:34 +0700

    initial commit

---
+++ OOP/text_based_adventure_game/rpginfo.py
@@ -0,0 +1,23 @@
+class RPGInfo() : 
+    # define class variable 
+    author = "Khoa" # name of the author who made the game 
+    def __init__(self,title_game) : 
+        self.title = title_game 
+
+    def welcome(self) : 
+        print("Welcome to " + self.title_game)
+
+    @staticmethod 
+    def info() : 
+
+        print("Made using the OOP game creator (c) me ")
+    # using classmethod allow you to interact with class variables like overriding in other programming languages 
+    @classmethod 
+    def credits(cls) :
+        print("Thank you for playing")
+        print("Created by " + cls.author) 
+
+    
+    
+
+
